96776d1b64e3ae08ffcf31752cd00eb2d7c86732
Adam <adam1092002@icloud.com>
2023-07-17 22:50:17 +0100

    Add project files.

---
+++ Finance.Web/Controllers/InterestCalculator.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Finance.Web.Controllers
+{
+    public class InterestCalculator : Controller
+    {
+        static public double deposit = 1;
+        static public double interestRate = 0;
+        static public double numberOfYears = 0;
+
+        public IActionResult Index()
+        {
+
+          
+            ViewBag.Deposit = deposit;
+            ViewBag.InterestRate = interestRate;
+            ViewBag.NumberOfYears = numberOfYears;
+
+            return View();
+        }
+
+
+        public IActionResult Calcs()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Calcs(int addeposit, int years, int rate)
+        {
+            deposit = addeposit;
+            interestRate = rate;
+            numberOfYears = years;
+
+            ViewBag.Deposit = deposit;
+            ViewBag.InterestRate = interestRate;
+            ViewBag.NumberOfYears = numberOfYears;
+            return RedirectToAction("interestDisplay");
+        }
+
+        public IActionResult interestDisplay()
+        {
+
+
+            ViewBag.Deposit = deposit;
+            ViewBag.InterestRate = interestRate;
+            ViewBag.NumberOfYears = numberOfYears;
+
+            double totalAmount = 0;
+
+            totalAmount = deposit * Math.Pow((1 + (interestRate/100) / 12), (12 * numberOfYears));
+            ViewBag.TotalAmount = totalAmount;
+            return View();
+        }
+
+        
+    }
+}
